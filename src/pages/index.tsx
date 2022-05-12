fb3d23587bd20b34fde852ca0a35850f178ddf81
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-13 00:58:41 +0900

    ✨ feat: style buttons and trpc url gen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,19 +24,35 @@ const Home: NextPage = () => {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selected: number) => {
+    updateIds(getOptionsForVote());
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img src={firstPokemon.data?.sprites.front_default ?? ""} alt="first pokemon" className="w-full" />
           <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+          <button
+            className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={() => voteForRoundest(first)}
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img src={secondPokemon.data?.sprites.front_default ?? ""} alt="second pokemon" className="w-full" />
           <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+          <button
+            className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={() => voteForRoundest(second)}
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-2" />
       </div>
