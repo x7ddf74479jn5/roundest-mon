fb2e049291acd58a8420225ce5f4975915dd714b
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-12 23:55:51 +0900

    ✨ faet: add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,19 @@
 import type { NextPage } from "next";
 
 import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "world" }]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
+  const [first, second] = getOptionsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-pink-400"></div>
+        <div className="w-16 h-16 bg-red-200">{second}</div>
       </div>
     </div>
   );
